cca0657ec14e5b0e97134fefc85d7f1f1d949809
Luciano Blanco <luciano.blanco@live.u-tad.com>
2023-11-21 13:37:52 +0100

    21/11 recycler ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
         enable = true
     }
 }
-
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -54,6 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
 
     //Navigation
